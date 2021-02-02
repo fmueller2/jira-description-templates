77b189a7b69fb70b5a7eb0bad16d206dfd9dbabf
Achim Wolf Sperling <achim.sperling@bike24.net>
2021-02-02 08:37:34 +0100

    Provide build for 0.1.0

---
+++ dist/jira-description-templates.meta.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        jira-description-templates
+// @version     0.1.0
+// @author      Achim Sperling <achim.sperling@bike24.net>
+// @description A tampermonkey/greasemonkey plugin for Jira
+// @namespace   https://dev.bike24.io
+// @include     https://jira.bike24.net/*
+// ==/UserScript==
